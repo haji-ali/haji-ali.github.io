1fc779d34ae5dbb30a315413e2f5a9cb280861a5
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2020-11-11 10:54:49 +0000

    Added PhD position advertisment

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Fully-funded PhD Position",
+        "excerpt":"The Department of Statistics and Actuarial Mathematics at Heriot-Watt University and the British Antarctic Survey invites applications for a PhD position in “Statistical Control of the Ecological Risks of Fisheries” under the primary supervision of Abdul-Lateef Haji-Ali from HWU and Co-Supervision of Simeon Hill and Norman Ratcliffe from BAS. Successful...","categories": [],
+        "tags": [],
+        "url": "/posts/phd-bas-advert",
+        "teaser": null
+      }]
