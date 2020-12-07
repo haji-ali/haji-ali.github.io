ed4cb73f35c23c9929d1c7e50c564b194fe2341a
Al Haji-Ali <a.hajiali@hw.ac.uk>
2020-12-07 16:57:52 +0000

    Fixed a mistake

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Fully-funded PhD Position: _Statistical Control of the Ecological Risks of Fisheries_",
-        "excerpt":"The Department of Statistics and Actuarial Mathematics at Heriot-Watt University and the British Antarctic Survey invites applications for a PhD position for the project “Statistical Control of the Ecological Risks of Fisheries” under the primary supervision of Abdul-Lateef Haji-Ali from HWU and Co-Supervision of Simeon Hill and Norman Ratcliffe from...","categories": [],
+        "excerpt":"The Department Actuarial Mathematics and Statistics at Heriot-Watt University and the British Antarctic Survey invites applications for a PhD position for the project “Statistical Control of the Ecological Risks of Fisheries” under the primary supervision of Abdul-Lateef Haji-Ali from HWU and co-Supervision of Simeon Hill and Norman Ratcliffe from BAS...","categories": [],
         "tags": [],
         "url": "/posts/phd-bas-advert",
         "teaser": null
